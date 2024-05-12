faf0a745d468b10e7256e8a708862983f3977eda
Mostafa Aliakbarzadeh <mostafa.aliakbarzade@vetted.work>
2024-05-12 13:36:38 +0330

    fix getPrice function

---
+++ tests/services/post_reader_service/test_getPrice.py
@@ -0,0 +1,17 @@
+from app.services.post_reader_service import PostReaderService
+
+
+def test_getPrice():
+    caption = "قیمت💰: 448 توما"
+    print(PostReaderService.getPrice(caption))
+    assert PostReaderService.getPrice(caption) == 448000
+    caption = "قیمت💰: 448 توما\nقیمت💰: 448 توما"
+    assert PostReaderService.getPrice(caption) == 448000
+    caption = "قیمت💰: 448 توما\nقیمت💰: 448 توما\nقیمت💰: 448 توما"
+    assert PostReaderService.getPrice(caption) == 448000
+    caption = "قیمت💰: 448 تومان موجودی 10 عدد"
+    assert PostReaderService.getPrice(caption) == 448000
+    caption = "قیمت💰: 4.48 تومان موجودی 10 عدد"
+    assert PostReaderService.getPrice(caption) == 4480
+    caption = "قیمت💰: 4,48 تومان موجودی 10 عدد"
+    assert PostReaderService.getPrice(caption) == 4480
